a07c02f692735ca9f52eb6db1d21ee5cf67918d9
Dharmendra songira <dharmendrasongira259@gmail.com>
2025-04-22 13:00:59 +0530

    fixed mistake in routing

--- src/lib/apiRequest.js
+++ src/lib/apiRequest.js
@@ -1,7 +1,7 @@
 import axios from "axios";
 
 const apirequest = axios.create({
-  baseURL: "http://localhost:3000/api",
+  baseURL: "https://homisphere-api.onrender.com/api",
   withCredentials: true,
 });
 export default apirequest;
